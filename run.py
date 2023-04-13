bd307885ec891a9d68a9f250c69bff28445be2bc
Nazek-Altayeb <n.almalaika@yahoo.com>
2023-04-13 21:38:16 +0000

    add project logo and options list

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import requests
 import time
+from os import system, name
 from termcolor import colored
 
 
@@ -12,6 +13,71 @@ HEADERS = {
     "X-RapidAPI-Host": "open-weather13.p.rapidapi.com"
     }
 Celsius = 273.15
+
+
+# def get_user_inputs():
+#     """
+#     Get user name and the name of the city s/he is at
+#     """
+#     print("Welcome to Plan_For_A_picnic project.\n")
+#     time.sleep(2)
+#     print("We are going to ask you couple of questions .\n")
+#     time.sleep(2)
+#     print("Lets start.\n")
+#     time.sleep(2)
+#     print(colored("What is your name?", "yellow"))
+#     while True:
+#         try:
+#             user_name = input(colored("Enter name: ", "green")).capitalize()
+#             city = input('please ' + user_name + ' enter the name of the city: ')
+#             request_url = f"{BASE_URL}?appid={API_KEY}&q={city}"
+#             response = requests.get(request_url)
+
+#             if response.status_code == 200:
+#                 data = response.json()
+#                 weather = data['weather'][0]['description']
+#                 temperature = round(data['main']['temp'] - Celsius, 2)
+#                 print('The weather in ' + city + ' is ' + weather)
+#                 print("and the temperature : ", temperature, "Celsius")
+#                 break
+#             else:
+#                 print(colored('An error accurred', 'red'))
+#         except ValueError as e:
+#             print(colored(f"Invalid entry: {e}\n", "red", attrs=['bold']))
+
+def start():
+    """
+    Load the project logo
+    """
+    clear_console()
+
+    print("""
+         _____            _____
+        |  __ \\ ||     ||     ||  ||\\   ||
+        | |__) | ||     ||     ||  || \\  ||
+        |  ___/  ||     ||_____||  ||  \\ ||
+        | |      ||     ||     ||  ||   \\||
+        |_|      ||___  ||     ||  ||    \\|
+        """.center(80))
+    time.sleep(2)
+    clear_console()
+    print("starting ....")
+    time.sleep(1)
+    clear_console()
+    options()
+
+
+def clear_console():
+    """
+    Clear screen
+    """
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
 
 
 def get_user_inputs():
@@ -28,27 +94,7 @@ def get_user_inputs():
     while True:
         try:
             user_name = input(colored("Enter name: ", "green")).capitalize()
-            city = input('please ' + user_name + ' enter the name of the city: ')
-            request_url = f"{BASE_URL}?appid={API_KEY}&q={city}"
-            response = requests.get(request_url)
-
-            if response.status_code == 200:
-                data = response.json()
-                weather = data['weather'][0]['description']
-                temperature = round(data['main']['temp'] - Celsius, 2)
-                print('The weather in ' + city + ' is ' + weather)
-                print("and the temperature : ", temperature, "Celsius")
-                break
-            else:
-                print(colored('An error accurred', 'red'))
-        except ValueError as e:
-            print(colored(f"Invalid entry: {e}\n", "red", attrs=['bold']))
-
-
-def test_rapid():
-    while True:
-        try:
-            city = input('please enter the name of the city: ')
+            city = input('please ' + user_name + ' enter the name of the city :')
             request_url = f"{BASE_URL}/{city}"
             response = requests.request("GET", request_url, headers=HEADERS)
             print(response.text)
@@ -58,8 +104,10 @@ def test_rapid():
                 data = response.json()
                 weather = data['weather'][0]['main']
                 temperature = data['main']['temp']
-                print('The weather in ' + city + ' is ' + weather)
-                print("and the temperature : ", temperature, "Celsius")
+                humidity = data['main']['humidity']
+                print('The weather in ' + city + ' is ' + weather + ',')
+                print("the temperature : ", temperature, "Celsius, ")
+                print("the humidity : ", humidity, "%.")
                 break
             else:
                 print(colored('An error accurred', 'red'))
@@ -67,9 +115,47 @@ def test_rapid():
             print(colored(f"Invalid entry: {e}\n", "red", attrs=['bold']))
 
 
+def learn_about_us():
+    """
+    Get user name and the name of the city s/he is at
+    """
+    print(colored("Welcome to Plan_For_A_picnic project.\n", "green"))
+    print(colored("The project gives advises according to the weather condition, \nand gives recommondation for an out door picnic in next 24 hours.\n", "green"))
+    print(colored("Please select one of the options below to proceed", "yellow"))
+    options()
+
+
+def options():
+    """
+    Display available options to the user
+    """
+    print("1 - Learn about plan_for_a_picnic")
+    print("2 - Search weather ")
+    print("Please type '1' or '2' below to select the related option")
+    select_option()
+
+
+def select_option():
+    """
+    get user's selected option
+    """
+    try:
+        option = int(input(""))
+        if option == 1:
+            learn_about_us()
+
+        elif option == 2:
+            main()
+
+        else:
+            raise ValueError
+    except ValueError:
+        print("Not a valid input. Please type '1' or '2' ", "your selection")
+        select_option()
+
+
 def main():
-    test_rapid()
-    # get_user_inputs()
+    get_user_inputs()
     
 
-main()
+start()
